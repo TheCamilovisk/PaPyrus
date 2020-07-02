96b31ab466c31e5d25ca49a60e2d2f2d3e79c0f0
Camilo Gonçalves <camilolgon@gmail.com>
2020-07-02 03:16:30 -0300

    Some adjustments to the test function.

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,8 +1,20 @@
 from papyrus.loaders import CBZLoader
+from pytest import fixture
 
 
-def test_load_cbz():
-    loader = CBZLoader()
-    hq = loader("sample/sample.cbz")
-    for item in hq.list_contents():
-        assert item in ["000.jpg", "001.jpg", "002.jpg", "003.jpg", "004.jpg"]
+@fixture(scope="function")
+def sample_contents():
+    return [
+        "000.jpg",
+        "001.jpg",
+        "002.jpg",
+        "003.jpg",
+        "004.jpg",
+    ]
+
+
+def test_load_cbz(sample_contents):
+    loader = CBZLoader("samples/sample.cbz")
+
+    for item in loader.get_pages_list():
+        assert item in sample_contents
